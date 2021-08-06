ae05bd5a61eb0aeb3a0e2b08adbfe2836f50e563
Felipe Amaral de Oliveira <felipe.deoliveira@ambevtech.com.br>
2021-08-06 17:50:43 -0300

    Implementação de um exemplo de API gateway com Ocelot

--- src/services/Gateway/Startup.cs
+++ src/services/Gateway/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MMLib.SwaggerForOcelot.DependencyInjection;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using System.IO;
@@ -24,6 +26,10 @@ namespace Gateway
                 .AddJsonFile("appsettings.json", true, true)
                 .AddJsonFile($"appsettings.{webHostEnvironment.EnvironmentName}.json", true, true)
                 .AddOcelot(ocelotConfigPath, webHostEnvironment)
+                .AddOcelotWithSwaggerSupport((x) =>
+                {
+                    x.Folder = ocelotConfigPath;
+                })
                 .AddEnvironmentVariables();
 
             Configuration = builder.Build();
@@ -33,6 +39,13 @@ namespace Gateway
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddOcelot(Configuration);
+            
+            services.AddSwaggerForOcelot(Configuration, (x) =>
+            {
+                x.GenerateDocsForAggregates = true;
+            });
+            
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Latest);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +56,15 @@ namespace Gateway
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
+            app.UseSwaggerForOcelotUI(opt =>
+            {
+                opt.PathToSwaggerGenerator = "/swagger/docs";
+            });
+
+            app.UseHttpsRedirection();
+            
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
